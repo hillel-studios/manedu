4ab60fa5f91347e5b9668217159bce3cd89d08df
Peter de Rooij <peter@Peters-Mac-mini.home>
2022-08-21 09:08:51 +0200

    Update htaccess to force https

---
+++ database/seeders/RolesPermissionsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RolesPermissionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $permissions = [];
+        DB::table('permissions')->insert($permissions);
+
+        $roles = [];
+        DB::table('roles')->insert($roles);
+    }
+}
